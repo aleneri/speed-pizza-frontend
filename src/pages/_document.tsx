4fdf9bbf2784a93cd0ce71ca80069674c97eadfd
aleneri <alessandro.neri@gmail.com>
2022-07-25 17:36:09 -0300

    feat: project base

---
+++ src/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "../../node_modules/next/document";
+
+export default function MyDocument() {
+    return (
+        <Html>
+            <Head>
+            
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
